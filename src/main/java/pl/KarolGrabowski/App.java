99e0ff7d1973ae997f1d201746cc87c82dd1e285
Karol Grabowski <grabowski-karol@wp.pl>
2017-05-19 20:21:08 +0200

    Initial commit

---
+++ src/main/java/pl/KarolGrabowski/App.java
@@ -0,0 +1,13 @@
+package pl.KarolGrabowski;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
