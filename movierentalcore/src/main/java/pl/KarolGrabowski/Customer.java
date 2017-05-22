409083e0efefc7fda27670afe899680ab77c02fd
Karol Grabowski <grabowski-karol@wp.pl>
2017-05-22 17:39:55 +0200

    .gitignore

---
+++ movierentalcore/src/main/java/pl/KarolGrabowski/Customer.java
@@ -0,0 +1,69 @@
+package pl.KarolGrabowski;
+
+import java.util.Date;
+
+public class Customer {
+    private static int nextId = 0;
+
+    private int id;
+    private String pesel;
+    private String firstName;
+    private String lastName;
+    private String city;
+    private Date date;
+
+    public Customer(String pesel, String firstName, String lastName, String city, Date date) {
+        id = nextId;
+        nextId++;
+
+        this.date = date;
+        this.pesel = pesel;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
